141a09468b34f7ec188ba18d2ebe27c35e60651d
Nicolò Calcavecchia <ncalcavecchia@zendesk.com>
2014-10-12 23:14:39 +0200

    Fix required files

--- lib/forget_table.rb
+++ lib/forget_table.rb
@@ -1,6 +1,9 @@
 require 'redis'
 
-require 'forget_table/distribution'
 require 'forget_table/decay'
 require 'forget_table/decrementer'
+require 'forget_table/distribution'
+require 'forget_table/distribution_decrementer'
+require 'forget_table/distribution_keys'
 require 'forget_table/poisson'
+require 'forget_table/weighted_distribution'
