1297be3b658b7e6a774be09adfde410ea04d17f9
Nicolò Calcavecchia <ncalcavecchia@zendesk.com>
2014-10-12 21:02:55 +0200

    Refactor decrementer to do less stuff

--- lib/forget_table/decrementer.rb
+++ lib/forget_table/decrementer.rb
@@ -1,50 +1,46 @@
+require 'forget_table/distribution_decrementer'
+
 module ForgetTable
 
   # Implements the decay of values in the given distribution.
   class Decrementer
 
     # TODO: maybe avoid passing redis here
-    def initialize(redis, distribution_name, last_updated_key, hits_count_key)
+    def initialize(redis, last_updated_key, hits_count_key, weighted_distribution)
       @redis = redis
-      @distribution_name = distribution_name
       @last_updated_key = last_updated_key
       @hits_count_key = hits_count_key
+      @weighted_distribution = weighted_distribution
     end
 
     def run!
-
-      existing_values = get_existing_values
-      decremented_values = decrement(existing_values.map(&:last))
-      new_hits_count = decremented_values.inject(:+)
-
-      keys = existing_values.map(&:first)
-      new_values = decremented_values.zip(keys)
-
-      last_updated_at = Time.now.to_i
-      run_pipeline(new_values, new_hits_count, last_updated_at)
+      decremented_distribution = distribution_decrementer.decrement
+      run_pipeline(decremented_distribution)
     end
 
     private
 
-    attr_reader :redis, :distribution_name, :last_updated_key, :hits_count_key
+    attr_reader :redis, :last_updated_key, :hits_count_key, :weighted_distribution
 
-    def run_pipeline(new_values, new_hits_count, last_updated_at)
+    # Updates:
+    # 1. the weighted distribution with the new distribution
+    # 2. the total number of hits for the distribution with the new count
+    # 3. the last_updated_at value with the current time
+
+    def run_pipeline(distribution)
       redis.pipelined do
-        redis.zadd(distribution_name, new_values)
-        redis.set(hits_count_key, new_hits_count)
-        redis.set(last_updated_key, last_updated_at)
+        redis.zadd(distribution.name, distribution.bins.to_a.map(&:reverse))
+        redis.set(hits_count_key, distribution.hits_count)
+        redis.set(last_updated_key, Time.now.to_i)
       end
     end
 
-    def decrement(values)
-      last_updated = Integer(@redis.get(last_updated_key))
-      decay = Decay.new(values, last_updated)
-      decay.decayed_values
-    end
-
-    def get_existing_values
-      redis.zrevrange(distribution_name, 0, -1, with_scores: true)
+    def distribution_decrementer
+      last_updated_at = Integer(@redis.get(last_updated_key))
+      distribution_decrementer = DistributionDecrementer.new(
+        weighted_distribution: weighted_distribution,
+        last_updated_at: last_updated_at
+      )
     end
   end
-
 end
--- spec/forget_table/decrementer_spec.rb
+++ spec/forget_table/decrementer_spec.rb
@@ -7,64 +7,63 @@ describe ForgetTable::Decrementer do
 
   let(:last_updated_key) { "guitars_t" }
   let(:hits_count_key) { "guitars_z" }
+  let(:weighted_distribution) { double(:weighted_distribution) }
+
+  let(:distribution_decrementer) do
+    double(:distribution_decrementer, decrement: decremented_distribution)
+  end
+
+  let(:decremented_distribution) do
+    double(:decremented_distribution,
+           name: distribution_name,
+           bins: { "fender" => 10.0, "gibson" => 20.0 },
+           hits_count: 30.0,
+          )
+  end
 
   let(:decrementer) do
     ForgetTable::Decrementer.new(
       redis,
-      distribution_name,
       last_updated_key,
-      hits_count_key
+      hits_count_key,
+      weighted_distribution
     )
   end
 
-  class FakeDecay
-    attr_reader :decayed_values
-    def initialize(args)
-      @decayed_values = args.map { |v| v - 1 }
-    end
-  end
-
   before(:each) do
-    allow(ForgetTable::Decay).to receive(:new) { |arg| FakeDecay.new(arg) }
     redis.flushall
+    redis.set(last_updated_key, 123)
+
+    allow(ForgetTable::DistributionDecrementer).to receive(:new).
+      with(
+        weighted_distribution: weighted_distribution,
+        last_updated_at: 123
+    ) { distribution_decrementer }
   end
 
-  describe "#run" do
-    it "raises an exception if not distribution is associated with this key" do
-      expect { decrementer.run }.to raise_error
+  describe "#run!" do
+    it "updates the `hits_count` key after decrementing" do
+      decrementer.run!
+
+      expect(redis.get(hits_count_key).to_i).to eq(30)
     end
 
-    context "with data" do
-      before do
-        redis.zincrby(distribution_name, 10, "fender")
-        redis.zincrby(distribution_name, 5, "gibson")
-        redis.set(hits_count_key, 15)
-        redis.set(last_updated_key, 123)
-      end
+    it "updates the timestamp" do
+      allow(Time).to receive_message_chain(:now, :to_i) { 37 }
 
-      it "updates the `hits_count` key after decrementing" do
-        decrementer.run
+      decrementer.run!
 
-        expect(redis.get(hits_count_key).to_i).to eq(13)
-      end
+      expect(redis.get(last_updated_key).to_i).to eq(37)
+    end
 
-      it "updates the timestamp" do
-        allow(Time).to receive_message_chain(:now, :to_i) { 37 }
+    it "stores the new values for the distribution" do
+      decrementer.run!
 
-        decrementer.run
-
-        expect(redis.get(last_updated_key).to_i).to eq(37)
-      end
-
-      it "stores the new values for the distribution" do
-        decrementer.run
-
-        expect(redis.zrevrange(distribution_name, 0, -1, with_scores: true)).to eq(
-          [
-            ["fender", 9.0],
-            ["gibson", 4.0],
+      expect(redis.zrevrange(distribution_name, 0, -1, with_scores: true)).to match_array(
+        [
+          ["fender", 10.0],
+          ["gibson", 20.0],
         ])
-      end
     end
   end
 end
