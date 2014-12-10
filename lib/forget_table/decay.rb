ca2f9a09bac7fe6e58df53f553a20f8b1d7bb8f6
Nicolò Calcavecchia <ncalcavecchia@zendesk.com>
2014-12-10 11:54:13 +0100

    Increase default value for decay_rate

--- lib/forget_table/decay.rb
+++ lib/forget_table/decay.rb
@@ -2,7 +2,7 @@ module ForgetTable
 
   class Decay
 
-    DEFAULT_DECAY_RATE = 0.05
+    DEFAULT_DECAY_RATE = 0.2
 
     # - last_updated: timestamp of the last update
     # - rate:         the rate of the decay (optional)
